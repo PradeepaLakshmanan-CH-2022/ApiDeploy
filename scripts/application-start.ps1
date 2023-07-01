3f65cb98b55803ea098ba0efdb57bf6d1f890305
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-01 16:53:58 +0530

    added scripts

--- scripts/application-start.ps1
+++ scripts/application-start.ps1
@@ -3,9 +3,14 @@
 # Install .NET runtime
 Start-Process -FilePath "dotnet-sdk-6.0.100-win-x64.exe" -ArgumentList '/install', '/quiet' -Wait
 
+# application-start.ps1
+
+# Install .NET runtime (if necessary)
+# This assumes the .NET runtime is already installed on the EC2 instance
+
 # Change to the application directory
 Set-Location -Path "C:\WebApiFolder"
 
-# Start the API application
-$apiCommand = "dotnet run"  # Replace with the actual command to start your API
-Start-Process -FilePath "dotnet" -ArgumentList $apiCommand -NoNewWindow -Wait
+# Start the API application using the self-contained executable
+$apiExecutable = "C:\WebApiFolder\PipelineTesting"  # Replace with the actual path to your self-contained executable
+Start-Process -FilePath $apiExecutable -NoNewWindow
